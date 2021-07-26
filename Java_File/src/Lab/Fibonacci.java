3fcacf3a4ef01f05c91f00f5eb5c2e08542f9218
sourabh12yadav <yadav18sourabh@gmail.com>
2021-07-26 23:10:27 +0530

    Fibo Programm

---
+++ Java_File/src/Lab/Fibonacci.java
@@ -0,0 +1,24 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] sors) {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int a=0,b=1;
+		
+		for(int i=0;i<n;i++)
+		  {
+			System.out.println(a);
+			int c=a+b;
+			a=b;
+			b=c;
+			
+		  }
+		
+	}
+
+}
